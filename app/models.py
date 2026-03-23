3d38733c660df7eb9869be9385aa3f4bd79498f3
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-03-23 19:26:20 -0300

    feat: add User model

--- app/models.py
+++ app/models.py
@@ -6,3 +6,13 @@ from .database import db
 class UserRole(str, Enum):
     ADMIN = "admin"
     USER = "user"
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(10), nullable=False, default=UserRole.USER.value)
