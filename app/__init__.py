3b9d0620cb87995ba09dabc95871226a09435925
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-03-25 13:05:15 -0300

    feat: register routes blueprint in app factory

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,15 @@ from .models import User
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
+
+    from .models import User
+    from .routes import user_bp
+
+    app.register_blueprint(user_bp)
 
     with app.app_context():
         db.create_all()
