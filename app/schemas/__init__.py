94c6f1efe0b578bc10194156499846ff2211fddb
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-04-06 10:45:18 -0300

    feat(schemas): export schemas in __init__

---
+++ app/schemas/__init__.py
@@ -0,0 +1,28 @@
+from app.schemas.member_schemas import (
+    AddMemberBody,
+    MemberResponse,
+    UpdateMemberRoleBody,
+)
+from app.schemas.project_schemas import (
+    CreateProjectBody,
+    ProjectListResponse,
+    ProjectResponse,
+    UpdateProjectBody,
+)
+from app.schemas.task_schemas import (
+    CreateTaskBody,
+    MoveTaskBody,
+    TaskHistoryResponse,
+    TaskListResponse,
+    TaskResponse,
+    UpdateTaskBody,
+)
+from app.schemas.user_schemas import (
+    LoginBody,
+    LoginResponse,
+    RegisterBody,
+    UpdatePasswordBody,
+    UpdateUserBody,
+    UserListResponse,
+    UserResponse,
+)
